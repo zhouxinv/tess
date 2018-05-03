cc6234869d0b744ea8f12cc6007f8fa9e4920c49
song <song@songdeimac.local>
2018-05-03 10:05:22 +0800

    Initial Commit

---
+++ Stone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stone
+//
+//  Created by song on 2018/5/3.
+//  Copyright © 2018年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stone
+//
+//  Created by song on 2018/5/3.
+//  Copyright © 2018年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
